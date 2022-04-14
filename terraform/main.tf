719c2932403df0780f207b713ed4a281e3ef3234
Amith Kadavigere <amithkj@gmail.com>
2022-04-14 17:25:56 +0530

    Modified Terraform scripts specific to Singlestore requirements

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,18 @@
 locals {
-  azs                    = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs                    = ["us-east-1a", "us-east-1b"]
   environment            = "dev"
   kops_state_bucket_name = "${local.environment}-kops-state"
   // Needs to be a FQDN
-  kubernetes_cluster_name = "k8s-dev0.domain.com"
+  kubernetes_cluster_name = "k8s-dev0.singlestore.com"
   ingress_ips             = ["10.0.0.100/32", "10.0.0.101/32"]
-  vpc_name                = "${local.environment}-vpc"
+  vpc_name                = "${local.environment}-k8s-vpc"
 
   tags = {
     environment = "${local.environment}"
     terraform   = true
+    Owner       = "amithtkm"
+    Project     = "Kubernetes DR testing"
+    Cost Center = "PS"
   }
 }
 
